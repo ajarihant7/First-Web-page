615e3ffef8f9c9e636887098e0e5a2818129df77
arihantdj <32020428+arihantdj@users.noreply.github.com>
2017-09-19 08:51:53 +0530

    Add files via upload

---
+++ arihant jain webdevelopment.js
@@ -0,0 +1,35 @@
+﻿window.onload=function()
+
+{
+   READMOREButton();
+};
+
+
+function READMOREButton()
+{
+    var button = document.getElementsByClassName("read-more-button");
+    
+for(var i=0; i < button.length ; i++)
+{
+
+button[i].addEventListener
+('click',function()
+{
+
+var READMORE=this.parentElement.parentElement.getElementsByClassName("read-more")[0]
+
+if(this.style.display == "")
+
+        {    
+
+            this.style.display = "none";
+
+            READMORE.style.display="block";
+
+ }
+  
+      };    
+
+    }
+    
+}
